573c35776cf0f7b7f33ee7528432a17c7e4854f4
ppecheux <ppecheux@etu.utc.fr>
2018-04-13 15:44:22 +0200

    firt modif

--- test.pl
+++ test.pl
@@ -1,2 +1,8 @@
 long([],0).
 long([_|L],X):-long(L,RE),X is RE+1.
+
+%git clone [url repo]
+%git status //pour voir les documents présents dans le dossier rouge pour untracked (non added) et vert pour committed
+%git add test.pl // pour mettre le fichier dans le commit.
+%git commit -m "commentaire"
+%git push origin master
