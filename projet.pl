d8f48e0027973bc54e8a4a985d8aac5146043574
Leo Siegel <LeoSiegel@siegelle-hz-2.etu.utc>
2018-05-11 16:22:49 +0200

    tp du vendredi 11 mai

--- projet.pl
+++ projet.pl
@@ -1,13 +1,69 @@
 % Porojet de IA2
 
-Grille([ 
-[a1,a2,a3,a4,a5,a6,a7,a8,a9], 
-[b1,b2,b3,b4,b5,b6,b7,b8,b9],
-[c1,c2,c3,c4,c5,c6,c7,c8,c9], 
-[d1,d2,d3,d4,d5,d6,d7,d8,d9], 
-[e1,e2,e3,e4,e5,e6,e7,e8,e9], 
-[f1,f2,f3,f4,f5,f6,f7,f8,f9], 
-[g1,g2,g3,g4,g5,g6,g7,g8,g9], 
-[h1,ah2,h3,h4,h5,h6,h7,h8,h9], 
-[i1,i2,i3,i4,i5,i6,i7,i8,i9] ]).
+/*grille([ 
+a1,a2,a3,a4,a5,a6,a7,a8,a9, 
+b1,b2,b3,b4,b5,b6,b7,b8,b9,
+c1,c2,c3,c4,c5,c6,c7,c8,c9, 
+d1,d2,d3,d4,d5,d6,d7,d8,d9, 
+e1,e2,e3,e4,e5,e6,e7,e8,e9, 
+f1,f2,f3,f4,f5,f6,f7,f8,f9, 
+g1,g2,g3,g4,g5,g6,g7,g8,g9, 
+h1,h2,h3,h4,h5,h6,h7,h8,h9, 
+i1,i2,i3,i4,i5,i6,i7,i8,i9 ]).*/
+
+grille([ 
+4,1,5,6,3,8,9,7,2, 
+3,6,2,4,7,9,1,8,5,
+7,8,9,2,1,5,3,6,4, 
+9,2,6,3,4,1,7,5,8, 
+1,3,8,7,5,6,4,2,9, 
+5,7,4,9,8,2,6,3,1, 
+2,5,7,1,6,4,8,9,3, 
+8,4,3,5,9,7,2,1,6, 
+6,9,1,8,2,3,5,4,7 ]).
+
+grillecare([ul,um,ur,ml,mm,mr,dl,dm,dr]).
+ul([a1,a2,a3,b1,b2,b3,c1,c2,c3]).
+um([a4,a5,a6,b4,b5,b6,c4,c5,c6]).
+ur([a7,a8,a9,b7,b8,b9,c7,c8,c9]).
+ml([d1,d2,d3,e1,e2,e3,f1,f2,f3]).
+mm([d4,d5,d6,e4,e5,e6,f4,f5,f6]).
+mr([d7,d8,d9,e7,e8,e9,f7,f8,f9]).
+dl([g1,g2,g3,h1,h2,h3,i1,i2,i3]).
+dm([g4,g5,g6,h4,h5,h6,i4,i5,i6]).
+dr([g7,g8,g9,h7,h8,h9,i7,i8,i9]).
+
+grilleligne([lA,lB,lC,ld,lE,lF,lG,lH,lI]).
+lA([a1,a2,a3,a4,a5,a6,a7,a8,a9]).
+lB([b1,b2,b3,b4,b5,b6,b7,b8,b9]).
+lC([c1,c2,c3,c4,c5,c6,c7,c8,c9]). 
+ld([d1,d2,d3,d4,d5,d6,d7,d8,d9]). 
+lE([e1,e2,e3,e4,e5,e6,e7,e8,e9]). 
+lF([f1,f2,f3,f4,f5,f6,f7,f8,f9]). 
+lG([g1,g2,g3,g4,g5,g6,g7,g8,g9]). 
+lH([h1,h2,h3,h4,h5,h6,h7,h8,h9]). 
+lI([i1,i2,i3,i4,i5,i6,i7,i8,i9]).
+
+grillecolonne([col1,col2,col3,col4,col5,col6,col7,col8,col9]).
+col1([a1,b1,c1,d1,e1,f1,g1,h1,i1]).
+col2([a2,b2,c2,d2,e2,f2,g2,h2,i2]).
+col3([a3,b3,c3,d3,e3,f3,g3,h3,i3]).
+col4([a4,b4,c4,d4,e4,f4,g4,h4,i4]).
+col5([a5,b5,c5,d5,e5,f5,g5,h5,i5]).
+col6([a6,b6,c6,d6,e6,f6,g6,h6,i6]).
+col7([a7,b7,c7,d7,e7,f7,g7,h7,i7]).
+col8([a8,b8,c8,d8,e8,f8,g8,h8,i8]).
+col9([a9,b9,c9,d9,e9,f9,g9,h9,i9]).
+
+ValideSudoku(G):- ValideGrille(grilecolonne), ValideGrile(grilleligne), ValideGrille(grillecarre).
+
+ValideGrille([]):- !.
+ValideGrille([A|B]):- Valideliste(A), ValideGrille(B). 
+
+Valideliste([]):- !.
+Valideliste([X|A]):- ValideElement(X, A),Valideliste(A).
+
+ValideElement(X, [Y|Z]):- X\=Y, ValideElement (X,Z).
+
+
 
