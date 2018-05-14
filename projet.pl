0aecaeae4d2afd1245df6517f5d4ee0763134b42
ppecheux <ppecheux@etu.utc.fr>
2018-05-14 12:21:29 +0200

    idee getter

--- projet.pl
+++ projet.pl
@@ -1,25 +1,25 @@
 % Porojet de IA2
 
-/*grille([ 
-a1,a2,a3,a4,a5,a6,a7,a8,a9, 
+/*grille([
+a1,a2,a3,a4,a5,a6,a7,a8,a9,
 b1,b2,b3,b4,b5,b6,b7,b8,b9,
-c1,c2,c3,c4,c5,c6,c7,c8,c9, 
-d1,d2,d3,d4,d5,d6,d7,d8,d9, 
-e1,e2,e3,e4,e5,e6,e7,e8,e9, 
-f1,f2,f3,f4,f5,f6,f7,f8,f9, 
-g1,g2,g3,g4,g5,g6,g7,g8,g9, 
-h1,h2,h3,h4,h5,h6,h7,h8,h9, 
+c1,c2,c3,c4,c5,c6,c7,c8,c9,
+d1,d2,d3,d4,d5,d6,d7,d8,d9,
+e1,e2,e3,e4,e5,e6,e7,e8,e9,
+f1,f2,f3,f4,f5,f6,f7,f8,f9,
+g1,g2,g3,g4,g5,g6,g7,g8,g9,
+h1,h2,h3,h4,h5,h6,h7,h8,h9,
 i1,i2,i3,i4,i5,i6,i7,i8,i9 ]).*/
 
-grille([ 
-4,1,5,6,3,8,9,7,2, 
+grille([
+4,1,5,6,3,8,9,7,2,
 3,6,2,4,7,9,1,8,5,
-7,8,9,2,1,5,3,6,4, 
-9,2,6,3,4,1,7,5,8, 
-1,3,8,7,5,6,4,2,9, 
-5,7,4,9,8,2,6,3,1, 
-2,5,7,1,6,4,8,9,3, 
-8,4,3,5,9,7,2,1,6, 
+7,8,9,2,1,5,3,6,4,
+9,2,6,3,4,1,7,5,8,
+1,3,8,7,5,6,4,2,9,
+5,7,4,9,8,2,6,3,1,
+2,5,7,1,6,4,8,9,3,
+8,4,3,5,9,7,2,1,6,
 6,9,1,8,2,3,5,4,7 ]).
 
 grillecare([ul,um,ur,ml,mm,mr,dl,dm,dr]).
@@ -36,12 +36,12 @@ dr([g7,g8,g9,h7,h8,h9,i7,i8,i9]).
 grilleligne([lA,lB,lC,ld,lE,lF,lG,lH,lI]).
 lA([a1,a2,a3,a4,a5,a6,a7,a8,a9]).
 lB([b1,b2,b3,b4,b5,b6,b7,b8,b9]).
-lC([c1,c2,c3,c4,c5,c6,c7,c8,c9]). 
-ld([d1,d2,d3,d4,d5,d6,d7,d8,d9]). 
-lE([e1,e2,e3,e4,e5,e6,e7,e8,e9]). 
-lF([f1,f2,f3,f4,f5,f6,f7,f8,f9]). 
-lG([g1,g2,g3,g4,g5,g6,g7,g8,g9]). 
-lH([h1,h2,h3,h4,h5,h6,h7,h8,h9]). 
+lC([c1,c2,c3,c4,c5,c6,c7,c8,c9]).
+ld([d1,d2,d3,d4,d5,d6,d7,d8,d9]).
+lE([e1,e2,e3,e4,e5,e6,e7,e8,e9]).
+lF([f1,f2,f3,f4,f5,f6,f7,f8,f9]).
+lG([g1,g2,g3,g4,g5,g6,g7,g8,g9]).
+lH([h1,h2,h3,h4,h5,h6,h7,h8,h9]).
 lI([i1,i2,i3,i4,i5,i6,i7,i8,i9]).
 
 grillecolonne([col1,col2,col3,col4,col5,col6,col7,col8,col9]).
@@ -55,15 +55,32 @@ col7([a7,b7,c7,d7,e7,f7,g7,h7,i7]).
 col8([a8,b8,c8,d8,e8,f8,g8,h8,i8]).
 col9([a9,b9,c9,d9,e9,f9,g9,h9,i9]).
 
+%on considère que les lignes et le colonnes commencent à 0.
+
+indiceElement(I,J,N):-N is I*9+J.
+
+GetElement(_,_,[],_):-write("la case correspondante n'existe pas").
+GetElement(0,0,[X|_],X).
+GetElement(I,J,[_|L],X):-N is I*9+J-1, I2 is N/9, J2 is N-I2, getElement(I2,J2,[L],X).
+
+GetLigne(_,[],_):-write("la ligne correspondante n'existe pas").
+GetLigne(I,[G],[L]):- getElement(I,_,[G],X). %trouver un moyen de rassembler les resultats de cette requette dans une liste
+
+GetCol(_,[],_):-write("la colonne correspondante n'existe pas").
+GetLigne(J,[G],[L]):- getElement(_,J,[G],X).
+
+%LES CARRES SONT IDENTIFIES PAR LES INDICES DE LA CASE EN HAUT A GAUCHE DU CARRE
+GetCarre(I,J,[G],[L]):-getElement(I,J,G,Aij),getElement(I+1,J,G,Ai1j),getElement(I+2,J,G,Ai2j),
+                        getElement(I,J+1,G,Aij1),getElement(I+1,J+1,G,Ai1j1),getElement(I+2,J+1,G,Ai2j1),
+                        getElement(I,J+2,G,Aij2),getElement(I+1,J+2,G,Ai1j2),getElement(I+2,J+2,G,Ai2j2),
+                        L is [Aij,Ai1j,Ai2j,Aij1,Ai1j1,Ai2j1,Aij2,Ai1j2,Ai2j2].
+
 ValideSudoku(G):- ValideGrille(grilecolonne), ValideGrile(grilleligne), ValideGrille(grillecarre).
 
 ValideGrille([]):- !.
-ValideGrille([A|B]):- Valideliste(A), ValideGrille(B). 
+ValideGrille([A|B]):- Valideliste(A), ValideGrille(B).
 
 Valideliste([]):- !.
 Valideliste([X|A]):- ValideElement(X, A),Valideliste(A).
 
 ValideElement(X, [Y|Z]):- X\=Y, ValideElement (X,Z).
-
-
-
