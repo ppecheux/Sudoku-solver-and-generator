7dd5a4001bfd650cbf6d19caf434e5fbba7c5d18
Leo Siegel <leo.siegel@etu.utc.fr>
2018-06-08 15:38:26 +0200

    Replace projet.pl

--- projet.pl
+++ projet.pl
@@ -1,5 +1,7 @@
 % Porojet de IA2
-chiffre([1,2,3,4,5,6,7,8,9]).
+
+%Le zero est une case vide.
+chiffre([1,2,3,4,5,6,7,8,9,0]).
 
 /*grille([
 a1,a2,a3,a4,a5,a6,a7,a8,a9,
@@ -21,7 +23,29 @@ grille([
 5,7,4,9,8,2,6,3,1,
 2,5,7,1,6,4,8,9,3,
 8,4,3,5,9,7,2,1,6,
-6,9,1,8,2,3,5,4,7 ]).
+6,9,1,8,2,3,5,4,7]).
+
+grilleP([
+0,1,5,6,3,0,9,7,2,
+0,0,0,4,7,9,1,8,5,
+7,8,0,2,1,5,3,6,4,
+9,2,6,3,4,1,7,5,8,
+1,3,8,7,5,6,4,2,9,
+5,7,4,9,8,2,6,3,1,
+2,5,7,1,6,4,8,9,3,
+8,4,3,5,9,7,2,1,6,
+6,9,1,8,2,3,5,4,7]).
+
+grilleVide([
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0]).
 
 grillecare([ul,um,ur,ml,mm,mr,dl,dm,dr]).
 ul([a1,a2,a3,b1,b2,b3,c1,c2,c3]).
@@ -56,49 +80,262 @@ col7([a7,b7,c7,d7,e7,f7,g7,h7,i7]).
 col8([a8,b8,c8,d8,e8,f8,g8,h8,i8]).
 col9([a9,b9,c9,d9,e9,f9,g9,h9,i9]).
 
+
+
 %on considère que les lignes et le colonnes commencent à 0.
+/*nToij(0,0,0):-!.
+nToij(0,J,J):-J<9,!.
+nToij(I,J,N):-
+	I1 is 1+I,
+	N1 is N-9,
+	nToij(I1,J,N1),!.*/
 
-indiceElement(I,J,N):-N is I*9+J.
+nToij(I,J,N):-catch(I is N div 9, 
+        error(Err,_Context),
+        (write('instantiation_error'),fail)),J is N-I*9,!.
+nToij(I,J,N):-N is 9*I+J,!.
 
-getElement(_,_,[],_):-write("la case correspondante n'existe pas"),!.
+getElement2(I,J,G,X):- 
+	getElementN(N,G,X),	nToij(I,J,N),!.
+
+getElementN(0,[X|_],X).
+getElementN(N,[_|G],X):-
+	getElementN(N1,G,X),
+	N is N1+1.
+
+
+
 getElement(0,0,[X|_],X).
-getElement(I,J,[_|L],X):-N is I*9+J-1, I2 is N/9, J2 is N-I2, getElement(I2,J2,[L],X).
+getElement(0,J,[_|L],X):- 
+	J2 is J-1,
+	getElement(0,J2,L,X).
+getElement(I,J,[_,_,_,_,_,_,_,_,_|L],X):-
+	I2 is I-1,
+	getElement(I2,J,L,X),!.
 
-getLigne(_,[],_):-write("la ligne correspondante n'existe pas"),!.
-getLigne(I,[G],[L]):- setof(X, getElement(I,_,[G],X),L) . %trouver un moyen de rassembler les resultats de cette requette dans une liste
 
-getCol(_,[],_):-write("la colonne correspondante n'existe pas"),!.
-getLigne(J,[G],[L]):- setof(X,getElement(_,J,[G],X),L).
+
+setAij(A,0,0,[_|G],[A|G]):-!.
+setAij(A,0,J,[T|G],[T|Guptade]):-
+	J2 is J-1,
+	setAij(A,0,J2,G,Guptade),!.
+setAij(A,I,J,[T0,T1,T2,T3,T4,T5,T6,T7,T8|G],[T0,T1,T2,T3,T4,T5,T6,T7,T8|Guptade]):-
+	I2 is I-1,
+	setAij(A,I2,J,G,Guptade),!.
+
+
+remplirCase(G,G1):-			remplirCaseVide(G,G1,9).
+
+remplirCaseRan(G,G1):-			remplirCaseVideRan(G,G1,9).
+
+remplirCaseVide(G,G1,C):-
+							C>0,
+							getElement2(I,J,G,0),
+							setAij(C,I,J,G,G1),
+							getCol1(J,G1,Col),
+							valideliste(Col),
+							getLigne2(I,G1,Lig),
+							valideliste(Lig),
+							getCarreAIJ(I,J,G1,Car),
+							valideliste(Car).
+
+remplirCaseVide(G,G1,C):-
+	C >0,
+	C1 is C-1,
+	remplirCaseVide(G,G1,C1).
+
+remplirCaseVideRan(G,G1,C):-
+							C>0,
+							getElement2(I,J,G,0),
+							random_between(1, 9, R),
+							setAij(R,I,J,G,G1),
+							getCol1(J,G1,Col),
+							valideliste(Col),
+							getLigne2(I,G1,Lig),
+							valideliste(Lig),
+							getCarreAIJ(I,J,G1,Car),
+							valideliste(Car).
+
+remplirCaseVideRan(G,G1,C):-
+	C >0,
+	C1 is C-1,
+	remplirCaseVideRan(G,G1,C1).
+
+remplirCaseVideRandom(G,G1,C,List):-
+	C>0,
+	chercheElement(C, List, E),
+	getElement2(I,J,G,0),
+	setAij(E,I,J,G,G1),
+	getCol1(J,G1,Col),
+	valideliste(Col),
+	getLigne2(I,G1,Lig),
+	valideliste(Lig),
+	getCarreAIJ(I,J,G1,Car),
+	valideliste(Car).
+
+
+remplirGrille(G, Res):- 			remplirGrille2(G, G, Res).
+
+remplirGrille2(G, [], Res):- 		Res=G.
+
+remplirGrille2(G, [0|G1], Res):- 	remplirCaseRan(G,G2),
+									valideGrille(G2),
+									%listToSudoku(G2),
+									remplirGrille2(G2, G1, Res).
+
+remplirGrille2(G, [A|G1], Res):- 	A\==0,
+									remplirGrille2(G, G1, Res).
+
+remplirGrilleUtilisateur(G, Res):- 				remplirGrille2(G, G, Res).
+
+remplirGrilleUtilisateur2(G, [], Res):- 		Res=G.
+
+remplirGrilleUtilisateur2(G, [0|G1], Res):- 	remplirCase(G,G2),
+												valideGrille(G2),
+												%listToSudoku(G2),
+												remplirGrille2(G2, G1, Res).
+
+remplirGrille2(G, [A|G1], Res):- 	A\==0,
+									remplirGrille2(G, G1, Res).
+
+remplir(G, Res):- remplir2(G,G,Res).
+remplir2(G, [], Res):- 		Res=G, writeln('GAGNE').
+
+remplir2(G, [0|G1], Res):- 	repeat, writeln('Ligne a remplir:'),
+							readln([I|_]),
+							writeln('Colonne a remplir de la ligne '),
+							readln([J|_]),
+							writeln('Chiffre a mettre:'),
+							readln([C|_]),
+							I2 is I-1,
+							J2 is J-1,
+							setAij(C,I2,J2,G,G2),
+							valideGrille(G2),
+							listToSudoku(G2),
+							remplir2(G2, G1, Res).
+
+remplir2(G, [A|G1],Res):- 	A\==0,
+							remplir2(G, G1, Res). 
+
+vidageGrille(G, G, 0).
+vidageGrille(G, G1, N):- 	N>0, N1 is N-1,
+							random_between(0,8, I),
+							random_between(0,8, J),
+							setAij(0, I, J, G, G2),
+							vidageGrille(G2, G1, N1).
+
+sudoku():- 	grilleVide(G),
+			remplirGrille(G, G1),
+			%listToSudoku(G1),
+			writeln('Facile(0), difficile(1) ?'),
+			readln([X]),
+			N is 30+5*X,
+			vidageGrille(G1, G2, N),
+			listToSudoku(G2),
+			remplir(G2, Res).	
+
+resoudre():-
+
+
+solve(G,G):- \+getElement2(I,J,G,0),valideGrille(G),!.
+
+solve(G,G1):- remplirCase(G,Guptade), solve(Guptade,G1).
+
+
+getLigne2(0,[A,B,C,E,F,G,H,I,J|_],[A,B,C,E,F,G,H,I,J]):-!.
+getLigne2(I,[_,_,_,_,_,_,_,_,_|L],X):- 
+	I2 is I-1,
+	getLigne2(I2,L,X).
+
+getCol1(J,G,L):-getCol2(J,G,L,0).
+
+getCol2(_,_,[],81).
+getCol2(_,[],[],_).
+
+getCol2(J,[T|G],[T|L],C):- 
+	congru(C,9,J),!,
+	C1 is C+1,
+	getCol2(J,G,L,C1),!.
+
+getCol2(J,[T|G],[_,_,_,_,_,_,_,_|L],C):-
+	Rest=J+1,
+	congru(C,9,Rest),!,
+	C1 is C+8,
+	getCol2(J,G,L,C1).
+
+getCol2(J,[T|G],L,C):-
+	C1 is C+1,
+	getCol2(J,G,L,C1).
 
 %LES CARRES SONT IDENTIFIES PAR LES iNDICES DE LA CASE EN HAUT A GAUCHE DU CARRE
-getCarre(I,J,[G],[L]):-getElement(I,J,G,Aij),getElement(I+1,J,G,Ai1j),getElement(I+2,J,G,Ai2j),
-                        getElement(I,J+1,G,Aij1),getElement(I+1,J+1,G,Ai1j1),getElement(I+2,J+1,G,Ai2j1),
-                        getElement(I,J+2,G,Aij2),getElement(I+1,J+2,G,Ai1j2),getElement(I+2,J+2,G,Ai2j2),
-                        L is [Aij,Ai1j,Ai2j,Aij1,Ai1j1,Ai2j1,Aij2,Ai1j2,Ai2j2].
+%ATTENTION IL DONNE DEUX SOLUTIONS PUIS ENVOIE FALSE
+getCarre(I,J,G,[Aij,Ai1j,Ai2j,Aij1,Ai1j1,Ai2j1,Aij2,Ai1j2,Ai2j2]):-
+	getElement(I,J,G,Aij),getElement(I+1,J,G,Ai1j),getElement(I+2,J,G,Ai2j),
+    getElement(I,J+1,G,Aij1),getElement(I+1,J+1,G,Ai1j1),getElement(I+2,J+1,G,Ai2j1),
+    getElement(I,J+2,G,Aij2),getElement(I+1,J+2,G,Ai1j2),getElement(I+2,J+2,G,Ai2j2).
+%getCarreAIJ permet de revoyer le carre asocié à l élément en question CA marche
+getCarreAIJ(I,J,G,L):-
+	congru(I,3,0),
+	congru(J,3,0),
+	getCarre(I,J,G,L),!.
+
+getCarreAIJ(I,J,G,L):-
+	congru(I,3,0),
+	J1 is J-1,
+	getCarreAIJ(I,J1,G,L),!.
+getCarreAIJ(I,J,G,L):-
+	I1 is I-1,
+	getCarreAIJ(I1,J,G,L).
 
 
-%quel nom de variable pour le 3 e argument desetof dans validListe?
-valideGrille([]):- write("la grille est vide"),!.
-valideGrille([G]):- valideliste(setof(L,getLigne(_,G,L),M)),
-                    valideliste(setof(L,getCol(_,G,L),N)),
-                    getCarre(0,0,G,UL),getCarre(3,0,G,UM),getCarre(6,0,G,UR),
-                    getCarre(0,3,G,ML),getCarre(3,3,G,MM),getCarre(6,3,G,MR),
-                    getCarre(0,6,G,LL),getCarre(3,6,G,LM),getCarre(6,6,G,DR),
-                    valideliste([UL]),valideliste([UL]),valideliste([UL]),
-                    valideliste([UL]),valideliste([UL]),valideliste([UL]),
-                    valideliste([UL]),valideliste([UL]),valideliste([UL]).
+valideCarres(G):-
+	getCarre(0,0,G,UL),getCarre(3,0,G,UM),getCarre(6,0,G,UR),
+    getCarre(0,3,G,ML),getCarre(3,3,G,MM),getCarre(6,3,G,MR),
+    getCarre(0,6,G,LL),getCarre(3,6,G,LM),getCarre(6,6,G,DR),
+    valideliste(UL),valideliste(UM),valideliste(UR),
+    valideliste(ML),valideliste(MM),valideliste(MR),
+    valideliste(LL),valideliste(LL),valideliste(LL),!.
 
-valideliste([]):- !.
-valideliste([X|A]):- valideElement(X, A),valideliste(A).
+valideGrille(G):-
+	valideLignes(G,0),
+	valideCols(G,0),
+	valideCarres(G).
+
+valideCols(_,9):-!.
+valideCols(G,I):-
+	getCol1(I,G,L),
+	valideliste(L),
+	I1 is I+1,
+	valideCols(G,I1).
+
+valideLignes(_,9):-!.
+valideLignes(G,I):-
+	getLigne2(I,G,L),
+	valideliste(L),
+	I1 is I+1,
+	valideLignes(G,I1).
+
+
+%Complexite de O(n3) pour valide liste avec n=9 pour le sudoku 9*9.
+
+valideliste([]).
+valideliste([X|A]):- valideElementRapide(X, A),valideliste(A),!.
 
 valideElement(X, []).
-valideElement(X, [Y|Z]):- X\=Y,in(X,Chiffre), valideElement (X,Z).
+valideElement(X, [Y|Z]):- X\=Y,chiffre(C),in(X,C), valideElement(X,Z),!.
 
-in(X,[]):-write("l'element n'est pas dans la liste"),!.
-in(X,[X,_]).
-in(X,[Y,L]):-in(X,L).
+valideElementRapide(X,[]).
+valideElementRapide(0,_).
+valideElementRapide(X, [Y|Z]):-X\=Y,valideElementRapide(X,Z),!.
+in(X,[]):-fail.
+in(X,[X|_]):-!.
+in(X,[Y|L]):-in(X,L).
 
-  %%%%%IMPRESSION DU SUDOKU%%%%%%%
+entreMinMax(Min,X,Max):-X>=Min,X=<Max.
+
+
+
+%%%%%IMPRESSION DU SUDOKU%%%%%%%
 
 congru(N,M,R) :-
 	I is (N mod M),
@@ -124,7 +361,7 @@ vSeparator(A):-
 
 cvSeparator(A):-
 	congru(A,27,26),!,
-	nl, write('-------------------');
+	nl, write('+-----+-----+-----+');
 	true.
 
 listToSudoku(G) :-
@@ -137,3 +374,17 @@ listToSudoku(G) :-
  %listToSudoku([8,0,4,0,0,0,2,0,9]).
   %listToSudoku([8,0,4,0,0,0,2,0,9]).
   %getLigne(1,[8,0,4,0,0,0,2,0,9,0,0,9,0,0,0,1,0,0,1,0,0,3,0,2,0,0,7,0,5,0,1,0,4,0,8,0,0,0,0,0,3,0,0,0,0,0,1,0,7,0,9,0,2,0,5,0,0,4,0,3,0,0,8,0,0,3,0,0,0,4,0,0,4,0,6,0,0,0,3,0,1],L).
+%getElement(0,0,[8,0,4,0,0,0,2,0,9,0,0,9,0,0,0,1,0,0,1,0,0,3,0,2,0,0,7,0,5,0,1,0,4,0,8,0,0,0,0,0,3,0,0,0,0,0,1,0,7,0,9,0,2,0,5,0,0,4,0,3,0,0,8,0,0,3,0,0,0,4,0,0,4,0,6,0,0,0,3,0,1],X).
+%getElement(0,1,[8,0,4,0,0,0,2,0,9,0,0,9,0,0,0,1,0,0,1,0,0,3,0,2,0,0,7,0,5,0,1,0,4,0,8,0,0,0,0,0,3,0,0,0,0,0,1,0,7,0,9,0,2,0,5,0,0,4,0,3,0,0,8,0,0,3,0,0,0,4,0,0,4,0,6,0,0,0,3,0,1],X).
+%getElement(0,2,[8,0,4,0,0,0,2,0,9,0,0,9,0,0,0,1,0,0,1,0,0,3,0,2,0,0,7,0,5,0,1,0,4,0,8,0,0,0,0,0,3,0,0,0,0,0,1,0,7,0,9,0,2,0,5,0,0,4,0,3,0,0,8,0,0,3,0,0,0,4,0,0,4,0,6,0,0,0,3,0,1],X).
+%grille(L),listToSudoku(L),getElement(5,5,L,E),getLigne2(2,L,M),getCol1(2,L,C),getCarre(3,3,L,K).
+%valideElement(3, [1,2,4,5,6,7]).
+%in(1,[1,2,3,4,5,6,7,8,9]).
+%valideliste([1,2,3,4,5,6,7,8,9]).
+%grille(G),listToSudoku(G),insererAij(2,0,0,G,H),listToSudoku(H).
+%grille(G),chiffre(C),in(J,C),setof(X,getElement(0,J,G,X),L).
+% grille(G),setAij(0,0,0,G,G1),getElement2(I,J,G1,0),setAij(5,I,J,G1,G2).
+%grille(G),setAij(0,3,1,G,G1),getElement2(I,J,G1,0),setAij(5,I,J,G1,G2).
+%grilleVide(G),remplirCase(G,G1),remplirCase(G1,G2).
+%grille(G),solve(G,G1).
+%grille(G),getElement2(I,J,G,0),setAij(8,I,J,G,G1),getCol1(J,G1,Col),valideliste(Col).
